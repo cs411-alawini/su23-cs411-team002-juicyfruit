95876c3d216b20b7e73498f69aa5bf89e589f4e7
Afif Shomali <shomali2@illinois.edu>
2023-07-19 16:30:27 -0500

    Got connectiong to db working

--- app.py
+++ app.py
@@ -5,9 +5,9 @@ import sqlalchemy
 
 app = Flask(__name__)
 
+# Connecting to our MySql Database
 connector = Connector()
 
-# function to return the database connection
 def getconn() -> pymysql.connections.Connection:
     conn: pymysql.connections.Connection = connector.connect(
         "cs411-juicyfruit:us-central1:cs411-juicyfruit",
@@ -23,15 +23,22 @@ pool = sqlalchemy.create_engine(
     creator=getconn,
 )
 
+#Flask App Routes
+@app.route('/')
+def test():
 
-with pool.connect() as db_conn:
-    results = db_conn.execute(sqlalchemy.text("SELECT Name FROM User_Information LIMIT 9")).fetchall()
+    with pool.connect() as db_conn:
+        results = db_conn.execute(sqlalchemy.text("SELECT Name FROM User_Information LIMIT 9")).fetchall()
 
+    ret_list = []
 
-for row in results:
-    print(row)
+    for row in results:
+        ret_list.append(row[0])
 
-connector.close()
+    print(ret_list)
+    connector.close()
+
+    return jsonify(ret_list)
 
 
 
